59c11b4fc58d05ae28d1b5da29b4c483dc497500
GuyonT <thomas.guyonnet@outlook.fr>
2024-07-11 21:12:33 +0200

    Add mouse over fonction to allow drawing

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
+let gridSize = prompt("Choose your grid size", "16");
+
+
 const container = document.querySelector("section");
-const widthDiv = container.offsetWidth / 16;
-const heightDiv = container.offsetHeight / 16;
+let widthDiv = container.clientWidth / +gridSize;
+let heightDiv = container.clientHeight / +gridSize;
+let opacity = 0;
 
 
-function createGrid () {
-    for (i = 0; i < 256; i++) {
+    for (i = 0; i < (gridSize * gridSize); i++) { 
         const div = document.createElement("div");
         div.classList.add("div");
         div.style.height = heightDiv + "px";
         div.style.width = widthDiv + "px" ;
+        div.style.opacity = 0;
+        
+        div.addEventListener('mouseover', function() {
+            const currentOpacity = parseFloat(this.style.opacity);
+            const newOpacity = Math.min(currentOpacity + 0.05, 1); // Clamp to 1 (max)
+            this.style.opacity = newOpacity;
+        });
+
         container.appendChild(div);
     }
-}
 
-createGrid();
+
 
 
